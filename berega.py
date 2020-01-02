1ff83cf18689f0827bf08afc85e038e9b2cfd393
James0251 <den4643025@mail.ru>
2020-01-02 21:09:56 +0300

    Создал вывод 100 постов

--- berega.py
+++ berega.py
@@ -2,3 +2,15 @@ import requests
 
 token = 'c38aa09bc38aa09bc38aa09b74c3e44553cc38ac38aa09b9d84b30e048f8ec39a7eaba6'
 version = 5.103
+domain = 'beregaonline_tutaev'
+
+response = requests.get('https://api.vk.com/method/wall.get',
+                        params={
+                            'access_token': token,
+                            'v': version,
+                            'domain': domain,
+                            'count': 100
+                        })
+
+data = response.json()['response']['items']
+print(1)
