c1d08c86e3468a29a4e8dadc7aa02c15f2a89b9d
James0251 <den4643025@mail.ru>
2020-01-03 11:03:30 +0300

    Создал цикл while, с помощью которого будет парситься группа

--- berega.py
+++ berega.py
@@ -1,16 +1,24 @@
 import requests
+import time
 
 token = 'c38aa09bc38aa09bc38aa09b74c3e44553cc38ac38aa09b9d84b30e048f8ec39a7eaba6'
 version = 5.103
 domain = 'beregaonline_tutaev'
+count = 100
+offset = 0
 
-response = requests.get('https://api.vk.com/method/wall.get',
-                        params={
-                            'access_token': token,
-                            'v': version,
-                            'domain': domain,
-                            'count': 100
-                        })
+while offset < 100:
+    response = requests.get('https://api.vk.com/method/wall.get',
+                            params={
+                                'access_token': token,
+                                'v': version,
+                                'domain': domain,
+                                'count': count,
+                                'offset': offset
+                            })
+    offset += 100
+    data = response.json()['response']['items']
+    print(data)
+    time.sleep(0.5)
 
-data = response.json()['response']['items']
-print(1)
+
